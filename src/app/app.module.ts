2f30f25d6064c8df305aa5aa14f56b8b41ac2f32
YΛKΛ <hnudka@gmail.com>
2018-02-15 02:14:18 +0700

    add detail page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { SidebarLeftComponent } from './shares/sidebar-left/sidebar-left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidebarLeftComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shares/sidebar-left/sidebar-left.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-left',
+  templateUrl: './sidebar-left.component.html'
+})
+export class SidebarLeftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
