eab2b8cd74c9f6816e20bbc3fdf697b60d4664e1
YΛKΛ <hnudka@gmail.com>
2018-04-05 06:56:13 +0700

    add select

--- src/app/component/ka-select/ka-select.component.ts
+++ src/app/component/ka-select/ka-select.component.ts
@@ -1,14 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ka-select',
   templateUrl: './ka-select.component.html'
 })
 export class KaSelectComponent implements OnInit {
+  @Input() title;
+  @Input() model;
+  @Input() required;
+  @Input() optionList;
+
+  // add variable toggle
+  aClass;
+  // generate variable for label Id
+  gName;
 
   constructor() { }
 
   ngOnInit() {
+    this.RandomNum();
+    this.toggleBlur();
+    if (this.required === '') {
+      this.required = true;
+    }
+  }
+
+  // onClike
+  toggleClick() {
+    this.aClass = true;
+  }
+  // onBlur
+  toggleBlur() {
+    if (this.model === '' || this.model === undefined) {
+      this.aClass = false;
+    } else {
+      this.aClass = true;
+    }
+  }
+
+  // generate ramdom name for label Id
+  RandomNum() {
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    this.gName = '_G' + getRandomInt(1, 1000);
   }
 
 }
--- src/app/pages/forms/forms.component.ts
+++ src/app/pages/forms/forms.component.ts
@@ -9,6 +9,16 @@ export class FormsComponent implements OnInit {
   model = 'YAKA Chang';
   radio = 1;
 
+  selectList = [
+    {Id: 1, Value: 'Input'},
+    {Id: 2, Value: 'Checkbox'},
+    {Id: 3, Value: 'Radio'},
+    {Id: 4, Value: 'Textarea'},
+    {Id: 5, Value: 'Select'}
+  ];
+
+  models = { type : this.selectList[4]};
+
   constructor() { }
 
   ngOnInit() {
